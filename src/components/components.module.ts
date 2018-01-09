bd408ff2d8096d2459e131d9c00b784a022c46ab
Samuel Segura <samuel3107@gmail.com>
2018-01-09 02:59:09 +0100

    searcher by name

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FinderComponent } from './finder/finder';
-@NgModule({
-	declarations: [FinderComponent],
-	imports: [],
-	exports: [FinderComponent]
-})
-export class ComponentsModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,15 +11,33 @@ import { MapHelper } from "../../helpers/mapHelper";
 })
 export class HomePage {
   url = 'http://localhost:3001/restaurants';
+
   restaurants;
+  fullRestaurantList;
+
   constructor(
     public navCtrl: NavController,
     private http: HttpClient,
-    private mapHelper: MapHelper
-    ) {
+    private mapHelper: MapHelper) {
+    this.initializeItems()
+  }
+
+  initializeItems(){
     this.http.get(this.url).subscribe(data => {
-      this.restaurants = data;
+      this.fullRestaurantList = data;
+      this.restaurants = this.fullRestaurantList;
     });
+  }
+
+  getItems(ev: any) {
+    this.restaurants = this.fullRestaurantList;
+    let val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.restaurants = this.restaurants.filter((restaurant) => {
+        return (restaurant.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
   viewRestaurant(data){
